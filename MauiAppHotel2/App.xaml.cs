590707cf40b9e1661b7f2434235277577e0d2919
taica <taica@TALITA>
2024-11-05 15:00:52 -0300

    add recursos ao app

--- MauiAppHotel2/App.xaml.cs
+++ MauiAppHotel2/App.xaml.cs
@@ -7,7 +7,7 @@ namespace MauiAppHotel2
         {
             InitializeComponent();
 
-            MainPage = new AppShell();
+            MainPage = new NavigationPage(new Views.ContratacaoHospedagem());
         }
 
         protected override Window CreateWindow(IActivationState? activationState)
---
+++ MauiAppHotel2/Views/ContratacaoHospedagem.xaml.cs
@@ -0,0 +1,9 @@
+namespace MauiAppHotel2.Views;
+
+public partial class ContratacaoHospedagem : ContentPage
+{
+	public ContratacaoHospedagem()
+	{
+		InitializeComponent();
+	}
+}
